6381cf4a26c038dd6de0e6502845245ab3840588
bashbunni <bashbunni.io@gmail.com>
2022-01-07 18:16:13 -0800

    refactor: slightly reduce space complexity

--- validparentheses/valid_parentheses.go
+++ validparentheses/valid_parentheses.go
@@ -1,23 +1,18 @@
 package validparentheses
 
-import (
-	"strings"
-)
-
 func IsValidParentheses(s string) bool {
 	// first in, last out -> stack
 	// hold brackets in a stack, check if closing matches the value on the top of the stack
 	if len(s) % 2 != 0 {
 		return false
 	}
-	validOpeners := "([{"
 	sets := map[string]string{"(": ")", "[": "]", `{`: `}`}
 	var stack []string
 	for _, ch := range s {
-		if !strings.Contains(validOpeners, string(ch)) && len(stack) == 0 {
+		if _, ok := sets[string(ch)]; !ok && len(stack) == 0 {
 			// starts with a closing brace
 			return false
-		} else if strings.Contains(validOpeners, string(ch)) {
+		} else if _, ok := sets[string(ch)]; ok {
 			// it's an opening brace -> add to stack
 			stack = append(stack, string(ch))
 		} else {
